d4f31f21b47e43b986896bdc9abf8b5deea6ac54
Emil Huzell <65963728+EmilHuzell@users.noreply.github.com>
2021-06-04 13:41:41 +0200

    Adds sink boat functionality

--- BattlesAhips/BattlesAhips.cpp
+++ BattlesAhips/BattlesAhips.cpp
@@ -2,31 +2,41 @@
 //
 
 #include <iostream>
+bool board[10][10] = { false };
 
 int main()
 {
-    bool board[10][10] = {false};
+    
 
-    int piecesPlaced = 0;
+    int boatsDropped = 0;
     char place[3];
-    while (piecesPlaced < 5) 
+    while (boatsDropped < 5) 
     {
         std::cout << "Chose a place to drop a boat" << '\n';
         
         std::cin >> place;
 
-        std::cout << (int)place[0] << '\n';
-        board[place[0] - 97][place[1] - 49] = true;
-        piecesPlaced++;
+        
+        
+        board[place[1] - 48][place[0] - 97] = true;
+        boatsDropped++;
     }
+    while (boatsDropped > 0) {
+        std::cout << "Chose a place to drop a bomb" << '\n';
+        std::cin >> place;
 
-    std::cout << '\n';
-
-    for (int y = 0; y < 10; y++) {
-        for (int x = 0; x < 10; x++) {
-            std::cout << board[x][y] << '\n';
+        if (board[place[1] - 48][place[0] - 97]) 
+        {
+            board[place[1] - 48][place[0] - 97] = false;
+            std::cout << "You sank a boat" << '\n';
+            boatsDropped--;
         }
+        else {
+            std::cout << "You missed" << '\n';
+        }
+        
     }
+
 }
 
 
