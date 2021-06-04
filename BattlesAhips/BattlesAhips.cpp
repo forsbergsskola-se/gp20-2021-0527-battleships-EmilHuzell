612ca18a58762390b0da65b9ffa1b9729ea5fca0
Emil Huzell <65963728+EmilHuzell@users.noreply.github.com>
2021-06-04 16:31:22 +0200

    Adds multiplayer functionality and refactors

--- BattlesAhips/BattlesAhips.cpp
+++ BattlesAhips/BattlesAhips.cpp
@@ -2,41 +2,84 @@
 //
 
 #include <iostream>
-bool board[10][10] = { false };
+
+
+
+
+class Player {
+    public:
+        int boats = 5;
+        bool board[10][10] = { false };
+        void dropBomb(Player* Enemy) {
+            char place[3];
+            
+            std::cout << "Chose a place to drop a bomb" << '\n';
+            std::cin >> place;
+
+            if ((*Enemy).board[place[1] - 48][place[0] - 97])
+            {
+                board[place[1] - 48][place[0] - 97] = false;
+                (*Enemy).boats--;
+                std::cout << "You sank a boat! ";
+                
+            }
+            else {
+                std::cout << "You missed ";
+            }
+            std::cout << "The enemy has " << (*Enemy).boats << " left" << '\n' << '\n';
+        }
+        void placeBoats() {
+            char place[3];
+            for (int i = 0; i < boats; i++)
+            {
+                std::cout << "Chose a place to drop a boat" << '\n';
+                std::cin >> place;
+                board[place[1] - 48][place[0] - 97] = true;
+            }
+            
+        }
+};
 
 int main()
 {
+ 
+
+    Player player1;
+    player1.boats = 5;
     
+    Player player2;
+    player2.boats = 5;
 
-    int boatsDropped = 0;
     char place[3];
-    while (boatsDropped < 5) 
-    {
-        std::cout << "Chose a place to drop a boat" << '\n';
-        
-        std::cin >> place;
+    
+    std::cout << "Firstly, player 1 places your boats" << '\n';
+    player1.placeBoats();
+    std::cout << '\n';
 
-        
-        
-        board[place[1] - 48][place[0] - 97] = true;
-        boatsDropped++;
-    }
-    while (boatsDropped > 0) {
-        std::cout << "Chose a place to drop a bomb" << '\n';
-        std::cin >> place;
+    std::cout << "Now, player 2 place your boats" << '\n';
+    player2.placeBoats();
+    std::cout << '\n';
 
-        if (board[place[1] - 48][place[0] - 97]) 
-        {
-            board[place[1] - 48][place[0] - 97] = false;
-            std::cout << "You sank a boat" << '\n';
-            boatsDropped--;
+    Player* player2Pointer;
+    player2Pointer = &player2;
+
+    Player* player1Pointer;
+    player1Pointer = &player1;
+    while (true) {
+        std::cout << "Player 1" << '\n';
+        player1.dropBomb(player2Pointer);
+        if (player2.boats < 1) {
+            std::cout << "Player1 has won the game" << '\n';
+            break;
         }
-        else {
-            std::cout << "You missed" << '\n';
-        }
-        
-    }
 
+        std::cout << "Player 2" << '\n';
+        player2.dropBomb(player1Pointer);
+        if (player1.boats < 1) {
+            std::cout << "Player2 has won the game" << '\n';
+            break;
+        }
+    }
 }
 
 
